280f4b753bfe79200d1a3ba1f2fbbb57c4bd8c9c
Alex Bryan <tabryan1990@gmail.com>
2024-02-24 13:46:50 -0500

    recursive debug solved

--- problems/recursive-debug.js
+++ problems/recursive-debug.js
@@ -12,10 +12,15 @@ doForAll([], (x) => x * 2); // => []
 doForAll([1, 2, 3], (x) => x + 1); // => [2, 3, 4]
 doForAll(["a", "b", "c"], (x) => x.toUpperCase()); // => ["A", "B", "C"]
 ***********************************************************************/
-
+debugger
 function doForAll(arr, action) {
+  if(!arr.length) return arr;
   return [action(arr[0]), ...doForAll(arr.slice(1), action)];
 }
+
+console.log(doForAll([], (x) => x * 2)); // => []
+console.log(doForAll([1, 2, 3], (x) => x + 1)); // => [2, 3, 4]
+console.log(doForAll(["a", "b", "c"], (x) => x.toUpperCase())); // => ["A", "B", "C"]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = doForAll;
